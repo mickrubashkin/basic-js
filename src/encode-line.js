c293e502c449857020361a2cdbe9a5f9455801d8
MickRubashkin <mikhail.rubashkin@gmail.com>
2023-12-21 09:13:39 +0100

    add encode-line solution

--- src/encode-line.js
+++ src/encode-line.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require('../extensions/index.js')
 
 /**
  * Given a string, return its encoding version.
@@ -10,11 +10,47 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  let result = ''
+  let token = null
+  let count = 1
+
+  for (let i = 0; i < str.length; i += 1) {
+    if (token === null) {
+      token = str[i]
+      continue
+    }
+
+    if (token === str[i]) {
+      count += 1
+
+      if (i === str.length - 1) {
+        if (count > 1) {
+          result += count + token
+        } else {
+          result += token
+        }
+      }
+    }
+
+    if (str[i] !== token) {
+      if (count > 1) {
+        result += count + token
+      } else {
+        result += token
+      }
+      token = str[i]
+      count = 1
+
+      if (i === str.length - 1) {
+        result += token
+      }
+    }
+  }
+
+  return result
 }
 
 module.exports = {
-  encodeLine
-};
+  encodeLine,
+}
